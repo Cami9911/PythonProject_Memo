294c8660b7772b731a237032fc196721828aa4dd
Cami9911 <camelia.tirnovan@yahoo.com>
2020-12-21 21:03:27 +0200

    updates

--- memo_list/urls.py
+++ memo_list/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.addItem, name="addItem"),
+    path('about/', views.aboutPage, name="aboutPage"),
     path('deleteItem/<list_id>', views.deleteItem, name="deleteItem"),
     path('editItem/<list_id>', views.editItem, name="editItem"),
     path('markAsComplete/<list_id>', views.markAsComplete, name="markAsComplete"),
     path('markAsIncomplete/<list_id>', views.markAsIncomplete, name="markAsIncomplete"),
-    path('about/', views.aboutPage, name="aboutPage"),
 
 ]
